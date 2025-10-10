cfe54a987a5097220d4c42d79d122928c191d5f1
carvestar <fntl0326@gmail.com>
2025-10-10 15:15:55 +0900

    5주차 실습파일

--- ch05/common_function.py
+++ ch05/common_function.py
@@ -1,4 +1,7 @@
 import platform
+from matplotlib import rc
+import matplotlib.pyplot as plt
+
 
 # 현재 파이썬이 실행되는 플랫폼 확인
 def is_windows_platform():
@@ -16,3 +19,8 @@ def get_font_name():
         return 'LinuxFont?'
     else:
         return 'Malgun Gothic'
+
+# 한글 폰트 깨짐 처리
+def init_matplotlib():
+    rc('font', family = get_font_name())
+    plt.rcParams['axes.unicode_minus'] = False
--- ch05/sample01.py
+++ ch05/sample01.py
@@ -1,43 +1,44 @@
-import os
 import pandas as pd
+import os
 
-covid_file_path = '../ch04/data/owid-covid-data.csv'
-raw_df = pd.read_csv(covid_file_path)
+covid_file_name = '../ch04/data/owid-covid-data.csv'
+raw_df = pd.read_csv(covid_file_name, sep=',')
 
-# 원하는 컬럼만
-selected_columns = ['iso_code', 'location', 'date', 'total_cases', 'population']
+selected_columns = ['iso_code','location','date','total_cases','population']
 selected_df = raw_df[selected_columns]
+print('=' * 50)
+print(selected_df.head())
 
-#South Korea
-south_korea_df = selected_df[selected_df.location == 'South Korea']
-print('-'*50)
-print(south_korea_df.head())
+kor_df = selected_df[selected_df['location'] == 'South Korea']
+print('='*50)
+print(kor_df.head())
 
-#data 컬럼을 index로 지정
+#date컬럼을 index로 지정
 index_name = 'date'
-korea_date_index_df = south_korea_df.set_index(index_name)
-print('-'*50)
-print(korea_date_index_df.head())
+kor_index_df = kor_df.set_index(index_name)
+print('='*50)
+print(kor_index_df.head())
 
-#저장(dataframe -> csv파일로)
-korea_covid_csv_file_name = './data/covid_korea.csv'
-if os.path.exists(korea_covid_csv_file_name):
-    os.remove(korea_covid_csv_file_name)
-korea_date_index_df.to_csv('./data/covid_korea.csv',  sep = '|', encoding = 'utf-8')
+#대한민국코로나데이터 저장(csv -> covid_kor.csv)
+kor_covid_file_name = './data/covid_kor.csv'
+if os.path.exists(kor_covid_file_name):
+    os.remove(kor_covid_file_name)
+kor_index_df.to_csv(kor_covid_file_name)
+#kor_index_df.to_csv(kor_covid_file_name, sep='|', encoding='utf-8')
+#kor_index_df.to_csv(kor_covid_file_name, encoding='utf-8', sep='|')
 
-#locationd일 미국으로 하고, date를 인덱스로 설정하는 df
-#실습!!!!
+#미국 코로나 발생 데이터 -> df(usa_index_df)
+# -> 실습!!!!
 usa_df = selected_df[selected_df['iso_code'] == 'USA']
-print('-'*50)
+print('='*50)
 print(usa_df.head())
 
-# USA 데이터에 index 지정
-usa_date_index_df = usa_df.set_index(index_name)
-print('-'*50)
-print(usa_date_index_df.head())
+#usa데이터에 index지정
+usa_index_df = usa_df.set_index(index_name)
+print('='*50)
+print(usa_index_df.head())
 
-#저장(usa dataframe -> csv파일로 저장)
-usa_covid_csv_file_path = './data/covid_usa.csv'
-if os.path.exists(usa_covid_csv_file_path):
-    os.remove(usa_covid_csv_file_path)
-usa_date_index_df.to_csv(usa_covid_csv_file_path)
+usa_covid_file_name = './data/covid_usa.csv'
+if os.path.exists(usa_covid_file_name):
+    os.remove(usa_covid_file_name)
+usa_index_df.to_csv(usa_covid_file_name)
