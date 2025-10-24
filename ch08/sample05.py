ef5f9e968c5f47e2d00428997c7e5455cd9f6e76
carvestar <fntl0326@gmail.com>
2025-10-24 15:09:40 +0900

    8주차 실습파일

--- ch08/sample05.py
+++ ch08/sample05.py
@@ -8,29 +8,32 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 def get_covid_data(iso_code, rate_yn):
-    file_path = './data/common_covid.csv'
-
-    raw_df = pd.read_csv(file_path)
+    file_path = './data/covid_common.csv'
+    common_df = pd.read_csv(file_path)
 
     # 인구비율 기준국가는 미국(USA)에 해당하는 인구수
-    common_population_sr = raw_df[raw_df.iso_code == 'KOR']['population']
-    common_population = common_population_sr.iat[0]
+    common_population_sr = common_df[common_df['iso_code'] == 'USA']['population']
+    common_population_value = common_population_sr.iat[0]
 
-    #매개변수로 넘어온 iso_code 국가의 인구수
-    cur_population_sr = raw_df[raw_df.iso_code == iso_code]['population']
-    cur_population = cur_population_sr.iat[0]
+    # 현재 iso_code에 해당하는 인구수
+    cur_population_sr = common_df[common_df['iso_code'] == iso_code]['population']
+    cur_population_value = cur_population_sr.iat[0]
 
-    rate = round(common_population / cur_population, 2)
+    rate = round(common_population_value / cur_population_value, 2)
 
-    filter_df = raw_df[raw_df.iso_code == iso_code]
-    filter_index_df = filter_df.set_index('date')
+    filter_df = common_df[common_df['iso_code'] == iso_code]
+
+    # date 컬럼을 인덱스로 지정
+    index_name = 'date'
+    index_df = filter_df.set_index(index_name)
 
     if rate_yn:
-        return filter_index_df['total_cases'] # * rate
+        return index_df['total_cases'] * rate
     else:
-        return filter_index_df['total_cases']
+        return index_df['total_cases']
 #end-def
 
+# 절대적인 값에 의해
 kor_data = get_covid_data('KOR', False)
 usa_data = get_covid_data('USA', False)
 fra_data = get_covid_data('FRA', False)
@@ -38,17 +41,19 @@ gbr_data = get_covid_data('GBR', False)
 pol_data = get_covid_data('POL', False)
 index_data = kor_data.index
 
-covid_df = pd.DataFrame({
+data = {
     'KOR': kor_data,
     'USA': usa_data,
     'FRA': fra_data,
     'GBR': gbr_data,
-    'POL': pol_data,
-}, index = index_data)
-covid_df[:].plot.line(rot = 45)
+    'POL': pol_data
+}
+
+df = pd.DataFrame(data, index = index_data)
+df[:].plot.line(rot = 45)
 
 
-
+# 인구 비율에 맞게
 kor_data = get_covid_data('KOR', True)
 usa_data = get_covid_data('USA', True)
 fra_data = get_covid_data('FRA', True)
@@ -63,5 +68,6 @@ covid_df = pd.DataFrame({
     'GBR': gbr_data,
     'POL': pol_data,
 }, index = index_data)
+
 covid_df[:].plot.line(rot = 45)
 plt.show()
